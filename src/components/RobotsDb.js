ee3def8ad82b72e5ea81b37140fa295a14d26b5f
andre <andre_w3@yahoo.com>
2021-01-10 02:26:58 -0500

    fixed grid issue

--- src/components/RobotsDb.js
+++ src/components/RobotsDb.js
@@ -33,5 +33,33 @@ export const robots = [
     username: 'harpy',
     email: 'cynt@gmail.com'
 
-}
+},
+{
+    id: 6,
+    name: 'Bobby Shmerrrrr',
+    username: 'Bobby Hats',
+    email: 'BobbyS_52@yahoo.com'
+
+},
+{
+    id: 7,
+    name: 'Ronda Yikes',
+    username: 'Run Baby Run',
+    email: 'Ronda@yahoo.com'
+
+},
+{
+    id: 8,
+    name: 'Donald Bumbp',
+    username: 'Palpatine',
+    email: 'allthesmoke@yahoo.com'
+
+},
+{
+    id: 9,
+    name: 'Lebron James',
+    username: 'the chosen one',
+    email: 'akrinking@gmail.com'
+
+},
 ]
