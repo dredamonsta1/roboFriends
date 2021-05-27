e0e9ca47483f61ac6ef383f9937fbcf193b49b23
andre <andre_w3@yahoo.com>
2021-05-27 18:40:34 -0400

    adding search state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 import CardList from './components/CardList';
 import SearchBox from './components/SearchBox';
 import { robots } from '../src/components/RobotsDb';
-
-
-
-
-// const state = {
-//     robots: robots,
-//     searchfield: ''
-// }
 
 class App extends Component {
     constructor() {
@@ -20,77 +12,29 @@ class App extends Component {
         }
     }
 
-    onSearchChange(e) {
-        console.log(e.target.value);
-        const filterRobots = this.state.robots.filter(robot =>{
-            return robots.name.toLowerCase().includes(this.onSearchChange.toLowerCase);
-        })
+    onSearchChange = (e) => {
+        this.setState({ searchfield: e.target.value })    
     }
 
-
     render() {
+        const filterRobots = this.state.robots.filter(robots =>{
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase);
+        })
+        console.log(filterRobots);
 
         return (
             <div>
                 <h1>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={this.state.robots}/>
+                <CardList robots={filterRobots}/>
             </div>
-            );
-        }
+        );
+    }
 
 }
 
 
 
-// const App = () => {
 
-    // const [robot, setRobot] = useState(robots);
-
-
-    // const onSearchChange = (e) => {
-    //     console.log(e.target.value);
-    // }
-
-    
-    
-    
-//     return (
-//         <>
-//             <h1>RoboFriends</h1>
-//             <SearchBox searchChange={onSearchChange} />
-//             <CardList robots={robots}/>
-//         </>
-//     )
-// }
 
 export default App;
-
-
-// ************************blanked out class code***************
-
-// class App extends React.Component {
-//     render() {
-//         return (
-//             <div className="App">
-//                 <header className="App-header">
-//                     <img src={logo} className="App-logo" alt="logo" />
-//                         <p>
-//                             Edit <code>src/App.js</code> and save to reload.
-//                         </p>
-//                         <a
-//                             className="App-link"
-//                             href="https://react.org"
-//                             target="_blank"
-//                             rel="noopener noreferrer"
-//                         >
-//                             Learn React
-//                         </a>
-//                 </header>            
-//             </div>
-//         );
-//     }
-// }
-
-
-// <p>{this.props.greeting}</p>
